e0b1bfba5b8f3d53d5ca4c15e07d43f0a8bc51f3
Joshua J. Haller <phone@joshuajhaller.com>
2021-07-08 14:28:14 -0700

    Moved data JSON file to public folder and used useState, useEffect and useCallback to mimic an API call to retrieve the data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect, useCallback } from "react";
 import { BiCalendar } from "react-icons/bi";
 import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
 import AppointmentInfo from "./components/AppointmentInfo";
-import appointmentList from "./data.json";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      });
+  }, []);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
